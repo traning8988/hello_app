3b519f220065b064f1c0e25d356939d9a15602a8
Shota Nakamichi <s02178906@gmail.com>
2023-11-08 13:09:45 +0900

    helloを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
 
-    def goodbye
-        render html: "goodbye, world!"
-    end
+def goodbye
+    render html: "goodbye, world!"
+end
 
+def hello
+    render html: "hello,world"
 end
